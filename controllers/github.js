8b62d4aa06ecb52233d86aa406eb1418dba2cff3
xuyitao <xuyt@kunion.com.cn>
2017-09-26 14:40:20 +0800

    测试github登陆3

--- controllers/github.js
+++ controllers/github.js
@@ -7,18 +7,14 @@ exports.callback = function (req, res, next) {
 	var email = profile.emails && profile.emails[0] && profile.emails[0].value;
 	console.log('callback='+JSON.stringify(profile));
 	User.getUsersByGitId(profile.id).then(function (user) {
-		if(user) {
-			user.set('githubUsername', profile.username);
-			user.set('githubId', profile.id);
-			user.set('githubAccessToken', profile.accessToken);
-			user.set('avatar', profile._json.avatar_url);
-			user.set('email', email || user.email);
-			user.save();
-			res.saveCurrentUser(user);
-			res.redirect('/');
-		} else {
-
-		}
+		user.set('githubUsername', profile.username);
+		user.set('githubId', profile.id);
+		user.set('githubAccessToken', profile.accessToken);
+		user.set('avatar', profile._json.avatar_url);
+		user.set('email', email || user.email);
+		user.save();
+		res.saveCurrentUser(user);
+		res.redirect('/');
 	}).catch(function (err) {
 		next(err);
 	})
@@ -26,17 +22,6 @@ exports.callback = function (req, res, next) {
 
 
 exports.githubMiddle = function (accessToken, refreshToken, profile, cb) {
-	console.log('githubMiddle='+JSON.stringify(profile));
-  var email = profile.emails && profile.emails[0] && profile.emails[0].value;
-  User.getUsersByGitId(profile.id).then(function (user) {
-		user.set('githubUsername', profile.username);
-		user.set('githubId', profile.id);
-		user.set('githubAccessToken', profile.accessToken);
-		user.set('avatar', profile._json.avatar_url);
-		user.set('email', email || user.email);
-		user.save();
-		cb(null, user)
-	}).catch(function (err) {
-		cb(err)
-	})
+	profile.accessToken = accessToken;
+  cb(null, profile);
 };
